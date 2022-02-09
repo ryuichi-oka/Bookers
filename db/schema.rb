c47924f6c1a12290cf93c078f332e7a0f5287f34
EC2 Default User <ec2-user@ip-172-31-42-83.ec2.internal>
2022-02-09 10:37:10 +0000

    [fix]migrationファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_044747) do
+ActiveRecord::Schema.define(version: 2022_02_05_161914) do
 
   create_table "books", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
